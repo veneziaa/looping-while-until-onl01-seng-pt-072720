7c2887443ad6e302867dd3443798ea8413b59b0b
Venice Vargas <venicevargas1@live.com>
2020-08-04 04:01:07 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,10 @@
-def using_while 
+def using_while
   levitation_force = 6
-    while levitation_force < 10
-    put "Wingardium Leviosa"
+  while levitation_force < 10 
+  put "Wingardium Leviosa"
   levitation_force += 1
-  end
 end
+
+
 
 
